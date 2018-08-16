bdb317cb550bff8819f9ef3698b9d18de18ce0d3
Abdullah-A-Elsayed <abdullah.ali26@hotmail.com>
2018-08-16 03:33:00 +0200

    fix bug in project

--- email Collector Project/collector script.sh
+++ email Collector Project/collector script.sh
@@ -2,9 +2,12 @@
 path=$(pwd) # pwd only is just a string
 rm -rf result.txt
 rm -rf emails.txt
+best=0
+best_name=''
 declare -A developers
 while read link
 do
+if [[ "$link" = "http"* ]];then
     #replace '/' by ' '
     link_translated=`echo ${link}| tr '/' ' '`
     link_arr=($link_translated)
@@ -22,33 +25,33 @@ do
         if [[ "${log_line}" = "Author:"* ]]; then
             line_arr=($log_line)
             name=${line_arr[1]}
-            if [[ $(grep "${line_arr[2]}" "emails.txt") ]]; then
+            if [[ $(grep "${log_line}" "emails.txt") ]]; then
             #:
                num=${developers[$name]}
                developers[$name]=$(expr $num + 1)
-               echo '...'
+               echo '.....'
             else
-                developers[$name]=0
+                developers[$name]=1
                 echo "reading $log_line"
-                echo ${line_arr[2]} >> emails.txt
+                echo ${line_arr[@]} >> emails.txt
             fi
         fi
     done < "${proj}_log.txt"
     rm -rf ${proj}_log.txt
-    best=0
-    best_name=''
-    for result in ${!developers[@]}
+fi
+done < "github links.txt"
+
+for result in ${!developers[@]}
     do
         num=${developers[${result}]}
         echo "$result: $num times.">> result.txt
         if [[ $(expr $num) -gt $(expr $best) ]];then
-            echo $num
+            #echo $num
             best_name=$result
             best=$num
         fi
-    done
+done
     echo '========== Best Record =========' >> result.txt
     echo "       ${best_name}" >> result.txt
     echo '================================' >> result.txt
     echo " ################ SUCCEEDED  ################"
-done < "github links.txt"
